29df8822360bf83f5af06add4ac77b064858fd25
Andi <andireas99@gmx.ch>
2018-03-27 00:29:20 +0200

    Menu + Quit

--- ch/andi/sprachassistent1/src/gui/MyWindow.java
+++ ch/andi/sprachassistent1/src/gui/MyWindow.java
@@ -1,8 +1,17 @@
 package gui;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
+import main.Main;
 
 public class MyWindow extends JFrame {
 
@@ -10,6 +19,25 @@ public class MyWindow extends JFrame {
 		super("Sprachassistent");
 		setVisible(true);
 		setPreferredSize(new Dimension(1000, 800));
+		setJMenuBar(myMenuBar());
 		pack();
 	}
+	
+	private JMenuBar myMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menu = new JMenu("File");
+		JMenuItem exitItem =  new JMenuItem("Exit");
+		exitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
+		exitItem.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Main.quit = true;
+			}
+		});
+		menu.add(exitItem);
+		menuBar.add(menu);
+		return menuBar;
+	}
+	
 }
