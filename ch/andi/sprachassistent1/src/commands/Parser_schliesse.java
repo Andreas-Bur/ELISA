745e3f6ceb070647f08ed7bdaf93be382ca531c5
Andi <andireas99@gmx.ch>
2018-03-27 22:33:00 +0200

    Parser

--- ch/andi/sprachassistent1/src/commands/Parser_schliesse.java
+++ ch/andi/sprachassistent1/src/commands/Parser_schliesse.java
@@ -2,8 +2,13 @@ package commands;
 
 public class Parser_schliesse {
 
-	public Parser_schliesse() {
+	public Parser_schliesse(String input) {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public static void parse(String input) {
+		System.out.println("schliesse parser: "+input);
+		
 	}
 
 }
