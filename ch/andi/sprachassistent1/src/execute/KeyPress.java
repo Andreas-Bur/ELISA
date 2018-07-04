10f72bd1891779fc00be8da041781dc4a2105353
Andi <andireas99@gmx.ch>
2018-07-04 21:55:56 +0200

    key combinations

--- ch/andi/sprachassistent1/src/execute/KeyPress.java
+++ ch/andi/sprachassistent1/src/execute/KeyPress.java
@@ -1,5 +1,7 @@
 package execute;
 
+import java.util.Arrays;
+
 import com.sun.jna.platform.win32.BaseTSD;
 import com.sun.jna.platform.win32.User32;
 import com.sun.jna.platform.win32.WinDef;
@@ -18,19 +20,22 @@ public class KeyPress {
 		input.input.ki.dwExtraInfo = new BaseTSD.ULONG_PTR(0);
 	}
 
-	public void press(int key) {
-		System.out.println("KeyPress.press: "+key);
+	public void press(int... keys) {
+		System.out.println("KeyPress.press: " + Arrays.toString(keys));
 
 		releaseLControl();
 
-		input.input.ki.wVk = new WinDef.WORD(key);
-		input.input.ki.dwFlags = new WinDef.DWORD(0);
-		User32.INSTANCE.SendInput(new WinDef.DWORD(1), (INPUT[]) input.toArray(1), input.size());
+		for (int key : keys) {
+			input.input.ki.wVk = new WinDef.WORD(key);
+			input.input.ki.dwFlags = new WinDef.DWORD(0);
+			User32.INSTANCE.SendInput(new WinDef.DWORD(1), (INPUT[]) input.toArray(1), input.size());
+		}
 
-		input.input.ki.wVk = new WinDef.WORD(key);
-		input.input.ki.dwFlags = new WinDef.DWORD(2);
-		User32.INSTANCE.SendInput(new WinDef.DWORD(1), (INPUT[]) input.toArray(1), input.size());
-
+		for (int key : keys) {
+			input.input.ki.wVk = new WinDef.WORD(key);
+			input.input.ki.dwFlags = new WinDef.DWORD(2);
+			User32.INSTANCE.SendInput(new WinDef.DWORD(1), (INPUT[]) input.toArray(1), input.size());
+		}
 	}
 
 	private void releaseLControl() {
@@ -38,7 +43,7 @@ public class KeyPress {
 		input.input.ki.dwFlags = new WinDef.DWORD(2);
 		User32.INSTANCE.SendInput(new WinDef.DWORD(1), (INPUT[]) input.toArray(1), input.size());
 	}
-	
+
 	public static void main(String[] args) {
 		KeyPress press = new KeyPress();
 		press.press('\r');
