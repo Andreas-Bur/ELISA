e387f1b25d238bf0ac46e599c997bca5ee0e297f
Andi <andireas99@gmx.ch>
2018-08-07 22:36:28 +0200

    more/better alerts

--- ch/andi/sprachassistent1/src/gui/AlertController.java
+++ ch/andi/sprachassistent1/src/gui/AlertController.java
@@ -1,42 +1,37 @@
 package gui;
 
 import feedback.FeedbackController;
+import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
 public class AlertController {
 
 	public static void showSprachErrorDialog(String sprache, String name) {
-		
-		new Thread(new FeedbackController(TrayIconController.ERROR_ICON, 10000)).start();
-		Alert alert = new Alert(AlertType.ERROR);
-		alert.setTitle("Error Dialog");
-		alert.setHeaderText("Unbekannte Sprache");
-		alert.setContentText("Die Sprache \"" + sprache + "\" des Programs \"" + name + "\" konnte nicht erkannt werden.");
 
-		alert.showAndWait();
+		showErrorDialog("Unbekannte Sprache",
+				"Die Sprache \"" + sprache + "\" des Programs \"" + name + "\" konnte nicht erkannt werden.");
 	}
 
 	public static void showProgramPathErrorDialog(String name, String pfad) {
-		
-		new Thread(new FeedbackController(TrayIconController.ERROR_ICON, 10000)).start();
-		Alert alert = new Alert(AlertType.ERROR);
-		alert.setTitle("Error Dialog");
-		alert.setHeaderText("Unbekannte Datei");
-		alert.setContentText("Das Programm \"" + name + "\" konnte unter dem Pfad \"" + pfad + "\" nicht gefunden werden.");
 
-		alert.showAndWait();
+		showErrorDialog("Unbekannte Datei",
+				"Das Programm \"" + name + "\" konnte unter dem Pfad \"" + pfad + "\" nicht gefunden werden.");
 	}
-	
+
 	public static void showErrorDialog(String header, String content) {
-		
-		new Thread(new FeedbackController(TrayIconController.ERROR_ICON, 10000)).start();
-		Alert alert = new Alert(AlertType.ERROR);
-		alert.setTitle("Error Dialog");
-		alert.setHeaderText(header);
-		alert.setContentText(content);
 
-		alert.showAndWait();
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				new Thread(new FeedbackController(TrayIconController.ERROR_ICON, 10000)).start();
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setTitle("Error Dialog");
+				alert.setHeaderText(header);
+				alert.setContentText(content);
+
+				alert.showAndWait();
+			}
+		});
 	}
-
 }
